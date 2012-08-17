388c900e93971620fd3c025a0e80fec346197fb1
bugadani <bugadani@bugadani-inspiron>
2012-08-17 20:59:12 +0200

    Make form error list protected.

--- FormDescriptor.php
+++ FormDescriptor.php
@@ -24,7 +24,7 @@ class FormDescriptor implements iValidable
         'method' => 'POST'
     );
     public $token;
-    private $errors;
+    protected $errors;
 
     public function __construct($data = array())
     {
