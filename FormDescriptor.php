530a95128f3c2f5633564a29d0782c92d5dc9cbc
bugadani <bugadani@bugadani-inspiron>
2012-08-17 14:22:28 +0200

    Update headers. Added License file. Deleted .gitignore

--- FormDescriptor.php
+++ FormDescriptor.php
@@ -2,26 +2,9 @@
 
 /**
  * This file is part of the Miny framework.
+ * (c) Dániel Buga <daniel@bugadani.hu>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version accepted by the author in accordance with section
- * 14 of the GNU General Public License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package   Miny/Modules/Form
- * @copyright 2012 Dániel Buga <daniel@bugadani.hu>
- * @license   http://www.gnu.org/licenses/gpl.txt
- *            GNU General Public License
- * @version   1.0-dev
+ * For licensing information see the LICENSE file.
  */
 
 namespace Modules\Form;
@@ -45,10 +28,8 @@ class FormDescriptor implements iValidable
 
     public function __construct($data = array())
     {
-        if(!is_array($data)) {
-            if(!($data instanceof ArrayAccess && $data instanceof Traversable)) {
-                throw new InvalidArgumentException('Data should be an array or array-like object.');
-            }
+        if (!is_array($data) && !$data instanceof ArrayAccess && !$data instanceof Traversable) {
+            throw new InvalidArgumentException('Data should be an array or array-like object.');
         }
 
         if ($this->hasOption('name') && !empty($data)) {
