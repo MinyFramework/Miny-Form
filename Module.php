46b8c49017160f4b3048030b36e72afef053478d
Buga Dániel <daniel@bugadani.hu>
2013-10-22 13:36:58 +0200

    Refactor for more readable code.

--- Module.php
+++ Module.php
@@ -26,34 +26,32 @@ class Module extends \Miny\Application\Module
         if (!is_null($token)) {
             $fv->addMethodCall('setCSRFToken', $token);
         }
+        $button = function($url, $method, array $params = array()) use($app, $token) {
+            if (isset($params['form'])) {
+                $form_params = $params['form'];
+                unset($params['form']);
+            } else {
+                $form_params = array();
+            }
+            $form_params['action'] = $url;
+            $form_params['method'] = $method;
 
-        $app->getBlueprint('view_helpers')->addMethodCall('addMethod', 'button',
-                function($url, $method, array $params = array()) use($app, $token) {
+            $descriptor = new FormDescriptor;
+            $descriptor->token = $app->getValue($token);
 
-                    if (isset($params['form'])) {
-                        $form_params = $params['form'];
-                        unset($params['form']);
-                    } else {
-                        $form_params = array();
-                    }
-                    $form_params['action'] = $url;
-                    $form_params['method'] = $method;
-
-                    $descriptor = new FormDescriptor;
-                    $descriptor->token = $app->getValue($token);
-
-                    if (isset($params['src'])) {
-                        $descriptor->addField(new Image('button', $params['src'], $params));
-                    } else {
-                        $value = isset($params['value']) ? $params['value'] : NULL;
-                        $descriptor->addField(new Submit('button', $value, $params));
-                    }
-                    $form = new FormBuilder($descriptor);
-                    $output = $form->begin($form_params);
-                    $output .= $form->render('button');
-                    $output .= $form->end();
-                    return $output;
-                });
+            if (isset($params['src'])) {
+                $descriptor->addField(new Image('button', $params['src'], $params));
+            } else {
+                $value = isset($params['value']) ? $params['value'] : NULL;
+                $descriptor->addField(new Submit('button', $value, $params));
+            }
+            $form = new FormBuilder($descriptor);
+            $output = $form->begin($form_params);
+            $output .= $form->render('button');
+            $output .= $form->end();
+            return $output;
+        };
+        $app->getBlueprint('view_helpers')->addMethodCall('addMethod', 'button', $button);
     }
 
 }
